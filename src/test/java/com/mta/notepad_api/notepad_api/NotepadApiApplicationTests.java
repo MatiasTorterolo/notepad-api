7e99abf55483c426cd2bbd26763b7930440ce98e
MatiasTorterolo <mtorterolo.a@gmail.com>
2024-10-07 16:24:25 -0300

    first commit

---
+++ src/test/java/com/mta/notepad_api/notepad_api/NotepadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mta.notepad_api.notepad_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
