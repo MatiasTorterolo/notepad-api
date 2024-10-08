3a4f6b9116d5e7e4bcb00f92c37fff702350e4df
MatiasTorterolo <mtorterolo.a@gmail.com>
2024-10-08 17:40:59 -0300

    added entities

---
+++ src/main/java/com/mta/notepad_api/notepad_api/entities/NoteEntity.java
@@ -0,0 +1,65 @@
+package com.mta.notepad_api.notepad_api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notes")
+public class NoteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    private LocalDateTime creationDate;
+
+    private LocalDateTime lastUpdate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    public NoteEntity(String text, LocalDateTime creationDate, LocalDateTime lastUpdate, UserEntity user) {
+        this.text = text;
+        this.creationDate = creationDate;
+        this.lastUpdate = lastUpdate;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+}
---
+++ src/main/java/com/mta/notepad_api/notepad_api/entities/UserEntity.java
@@ -0,0 +1,41 @@
+package com.mta.notepad_api.notepad_api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
