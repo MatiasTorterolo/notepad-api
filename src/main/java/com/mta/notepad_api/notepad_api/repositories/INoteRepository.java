602ee024d4b65cdec00c0d25d398654e21939306
MatiasTorterolo <mtorterolo.a@gmail.com>
2024-10-14 17:37:02 -0300

    added security config

--- src/main/java/com/mta/notepad_api/notepad_api/repositories/INoteRepository.java
+++ src/main/java/com/mta/notepad_api/notepad_api/repositories/INoteRepository.java
@@ -1,9 +1,14 @@
 package com.mta.notepad_api.notepad_api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mta.notepad_api.notepad_api.entities.NoteEntity;
 
 public interface INoteRepository extends JpaRepository<NoteEntity, Long> {
 
+    @Query("SELECT n FROM NoteEntity n where n.user.id = :userId")
+    List<NoteEntity> findAllNotes(Long userId);
 }
--- src/main/java/com/mta/notepad_api/notepad_api/services/UserService.java
+++ src/main/java/com/mta/notepad_api/notepad_api/services/UserService.java
@@ -1,9 +1,12 @@
 package com.mta.notepad_api.notepad_api.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.mta.notepad_api.notepad_api.domain.User;
 import com.mta.notepad_api.notepad_api.dtos.UserDTO;
+import com.mta.notepad_api.notepad_api.entities.UserEntity;
 import com.mta.notepad_api.notepad_api.repositories.IUserRepository;
 
 @Service
@@ -12,13 +15,17 @@ public class UserService {
     @Autowired
     private IUserRepository iUserRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void register(UserDTO userDTO) {
 
-        try {
+        User user = new User(userDTO.getUsername(), userDTO.getEmail(), userDTO.getPassword(), true);
 
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
+        UserEntity userEntity = new UserEntity(user.getUsername(), user.getEmail(),
+                passwordEncoder.encode(user.getPassword()));
+
+        iUserRepository.save(userEntity);
 
     }
 
