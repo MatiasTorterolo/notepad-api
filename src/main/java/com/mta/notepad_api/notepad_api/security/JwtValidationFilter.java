62b89c7b7fa9c265b1e02350b9be501c033ce5ad
MatiasTorterolo <mtorterolo.a@gmail.com>
2024-10-22 11:44:19 -0300

    ending security filters

--- src/main/java/com/mta/notepad_api/notepad_api/security/JwtValidationFilter.java
+++ src/main/java/com/mta/notepad_api/notepad_api/security/JwtValidationFilter.java
@@ -3,11 +3,19 @@ package com.mta.notepad_api.notepad_api.security;
 import static com.mta.notepad_api.notepad_api.security.TokenJwtConfig.SECRET_KEY;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -27,14 +35,36 @@ public class JwtValidationFilter extends BasicAuthenticationFilter {
 
         String header = request.getHeader("Authorization");
 
-        if (header == null || header.startsWith("Bearer ")) {
+        if (header == null || !header.startsWith("Bearer ")) {
 
+            chain.doFilter(request, response);
             return;
         }
 
         String token = header.replace("Bearer ", "");
 
-        Claims claims = Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(token).getPayload();
+        try {
+
+            Claims claims = Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(token).getPayload();
+            String username = claims.getSubject();
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+                    null, Collections.emptyList());
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            chain.doFilter(request, response);
+
+        } catch (JwtException e) {
+
+            Map<String, String> body = new HashMap<>();
+
+            body.put("error", e.getMessage());
+
+            response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+            response.setStatus(401);
+            response.setContentType("application/json");
+        }
+
     }
 
 }
