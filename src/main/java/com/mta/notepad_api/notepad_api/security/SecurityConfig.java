e923c8e371e48d927b763ce9438094f562b6cb24
MatiasTorterolo <mtorterolo.a@gmail.com>
2024-10-23 13:08:23 -0300

    lil change

--- src/main/java/com/mta/notepad_api/notepad_api/security/SecurityConfig.java
+++ src/main/java/com/mta/notepad_api/notepad_api/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.mta.notepad_api.notepad_api.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,8 +34,8 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
         return httpSecurity.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/register", "/home", "/login")
-                .permitAll()
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/register").permitAll()
                 .anyRequest()
                 .authenticated())
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))
